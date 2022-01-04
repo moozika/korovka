0bf488cea57527564f288704ebc0741dd1fdb535
Nikolay Pomytkin <nikolayp2800@gmail.com>
2022-01-04 17:59:46 -0500

    removed heroku ci and updated test name

--- tests/test_root.py
+++ tests/test_root.py
@@ -5,7 +5,7 @@ from ..main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'status': 'available'}
